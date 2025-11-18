a4e5ff2266151b2690132f4c556a3c5dcb323daa
Anderson Wilmer <anderson.wilmer@gmail.com>
2025-11-17 19:50:43 -0500

    Comandos do terinal

---
+++ comandos.sh
@@ -0,0 +1,33 @@
+    1  poweroff
+    2  sudo apt install curl
+    3  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    4  sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+    5  sudo apt update
+    6  sudo apt install brave-browser
+    7  exit
+    8  sudo apt update
+    9  sudo apt install torbrowser-launcher -y
+   10  exit
+   11  sudo apt update
+   12  sudo apt install git vim notepadqq 
+   13  vim
+   14  sudo init 0
+   15  sudo apt update
+   16  sudo
+   17  sudo -i
+   18  sudo apt update 
+   19  sudo apt upgrade
+   20  sudo apt dist-upgrade
+   21  sudo apt full-upgrade 
+   22  sudo apt autoremove 
+   23  sudo apt autoclean 
+   24  sudo apt clean 
+   25  sudo apt update
+   26  sudo apt install git vim pip python3
+   27  git clone https://github.com/wilmeryf/linuxmint
+   28  git config --global user.name "Anderson Wilmer"
+   29  git config --global user.email anderson.wilmer@gmail.com
+   30  git config --global core.editor vim
+   31  git config --global merge.tool vimdiff
+   32  git config --global color.ui true
+   33  history
---
+++ comandos/README.md
@@ -0,0 +1,6 @@
+# linuxmint
+Integração do Git no GitHub no Linux Mint 22.2 Zara
+
+Anderson Wilmer
+
+Senac Tatuapé utilizando o Linux Mint
